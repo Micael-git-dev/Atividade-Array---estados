27e115077d7d736e2ad129fa51374d67d32b1bd1
Micael-git-dev <micaels3nac@gmail.com>
2024-08-30 21:02:43 -0300

    Add files via upload

---
+++ atividadearrayestado.php
@@ -0,0 +1,10 @@
+<?php
+
+    $estados = array("Brasília","Salvador", "Rio de Janeiro", "Curitiba", "Amazonas");
+    
+    echo $estados[0]."<br>";
+    echo $estados[1]."<br>";
+    echo $estados[2]."<br>";
+    echo $estados[3]."<br>";
+    echo $estados[4]."<br>";
+?>
