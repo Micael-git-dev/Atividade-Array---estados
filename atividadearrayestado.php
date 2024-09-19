3a73f8915260e62e85a8f3aed4617504a8d35783
Micael-git-dev <micaels3nac@gmail.com>
2024-09-18 21:30:09 -0300

    Update atividadearrayestado.php

--- atividadearrayestado.php
+++ atividadearrayestado.php
@@ -2,7 +2,7 @@
 
     $estados = array("Brasília","Salvador", "Rio de Janeiro", "Curitiba", "Amazonas");
     
-    echo $estados[0]."<br>";
+    echo $estados[0]."<br>";                //Duas formas de fazer
     echo $estados[1]."<br>";
     echo $estados[2]."<br>";
     echo $estados[3]."<br>";
